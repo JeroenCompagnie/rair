5dd2a8566da8a1e68d839fd5019440361bbaa53c
JeroenCompagnie <jeroen.compagnie@gmail.com>
2016-11-07 14:34:03 +0100

    register customer bean made

---
+++ src/main/java/com/realdolmen/domain/beans/RegisterCustomerBean.java
@@ -0,0 +1,13 @@
+package com.realdolmen.domain.beans;
+
+import javax.faces.bean.SessionScoped;
+import javax.inject.Named;
+
+@Named("registration")
+@SessionScoped
+public class RegisterCustomerBean {
+	
+	private String firstName;
+	
+	
+}
