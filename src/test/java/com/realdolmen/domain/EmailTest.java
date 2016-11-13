5eeaa43fc56f9dd0d04cf3ee32a65b6f3ebfa904
JeroenCompagnie <jeroen.compagnie@gmail.com>
2016-11-13 14:23:07 +0100

    email push

--- src/test/java/com/realdolmen/domain/EmailTest.java
+++ src/test/java/com/realdolmen/domain/EmailTest.java
@@ -1,13 +1,16 @@
 package com.realdolmen.domain;
 
+import java.util.Date;
+
 import org.junit.Test;
 
-import com.realdolmen.email.MailService;
+import com.realdolmen.email.Email;
 
 public class EmailTest {
 	@Test
 	public void sendEmail(){
-		MailService mailService = new MailService();
-		mailService.mail("jeroen.compagnie@gmail.com", "Java test", "This was sent from java :3");
+		Email email = new Email();
+		String[] recipients = {"jeroen.compagnie@gmail.com"};
+		email.sendMailStandardSender(recipients, "Java test", "This was sent from java :3 at " + new Date().toString());
 	}
 }
--- src/test/java/com/realdolmen/domain/flight/DiscountTest.java
+++ src/test/java/com/realdolmen/domain/flight/DiscountTest.java
@@ -2,6 +2,8 @@ package com.realdolmen.domain.flight;
 
 import static org.junit.Assert.*;
 
+import java.util.Date;
+
 import org.junit.Test;
 
 public class DiscountTest {
@@ -22,6 +24,41 @@ public class DiscountTest {
 		Double baseprice = 100.0;
 		
 		Double discountedPrice = discountRealvalue.addDiscountToPrice(baseprice);
+		
+		assertEquals(90.01,discountedPrice, 0.0001);
+	}
+	
+	@Test
+	public void testBasicPercentageDiscountInPeriodFuture(){
+		Date begin = new Date(2016,12,1);
+		Date end = new Date(2016,12,2);
+		Discount discountPercentage = new Discount(true, 0.05, true, begin, end);
+		Double baseprice = 100.0;
+		
+		Double discountedPrice = discountPercentage.addDiscountToPrice(baseprice);
+		
+		assertEquals(100.0,discountedPrice, 0.0001);
+	}
+	
+	@Test
+	public void testBasicPercentageDiscountInPeriodPast(){
+		Date begin = new Date(2016,11,1);
+		Date end = new Date(2016,11,2);
+		Discount discountPercentage = new Discount(true, 0.05, true, begin, end);
+		Double baseprice = 100.0;
+		
+		Double discountedPrice = discountPercentage.addDiscountToPrice(baseprice);
+		
+		assertEquals(100.0,discountedPrice, 0.0001);
+	}
+	
+	public void testBasicPercentageDiscountInRightPeriod(){
+		Date begin = new Date(2016,11,1);
+		Date end = new Date(2016,12,1);
+		Discount discountPercentage = new Discount(true, 0.05, true, begin, end);
+		Double baseprice = 100.0;
+		
+		Double discountedPrice = discountPercentage.addDiscountToPrice(baseprice);
 		
 		assertEquals(95.0,discountedPrice, 0.0001);
 	}
